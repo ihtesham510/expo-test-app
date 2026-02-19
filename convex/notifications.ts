c0e16cd33b9f84c1e5362b0566898499d520e08e
ihtesham510 <ihteshamulhaq510@gmail.com>
2026-02-19 16:44:17 +0500

    fix: notifications and added new internal query

--- convex/notifications.ts
+++ convex/notifications.ts
@@ -1,11 +1,10 @@
 import { PushNotifications } from '@convex-dev/expo-push-notifications'
 import { v } from 'convex/values'
 import { components } from './_generated/api'
-import type { Id } from './_generated/dataModel'
-import { mutation } from './_generated/server'
+import { internalMutation, mutation } from './_generated/server'
 import { getuserIdentity } from './auth'
 
-const pushNotifications = new PushNotifications<Id<'user'>>(components.pushNotifications, {
+const pushNotifications = new PushNotifications<string>(components.pushNotifications, {
 	logLevel: 'DEBUG',
 })
 
@@ -25,14 +24,43 @@ export const recordPushNotificationToken = mutation({
 export const sendPushNotification = mutation({
 	args: { title: v.string(), to: v.string() },
 	handler: async (ctx, args) => {
-		const normalizedId = ctx.db.normalizeId('user', args.to)
-		if (normalizedId) {
-			return await pushNotifications.sendPushNotification(ctx, {
-				userId: normalizedId,
-				notification: {
-					title: args.title,
-				},
-			})
-		}
+		return await pushNotifications.sendPushNotification(ctx, {
+			userId: args.to,
+			notification: {
+				title: args.title,
+				badge: 1,
+			},
+		})
+	},
+})
+
+export const sendInternalNotification = internalMutation({
+	args: {
+		title: v.string(),
+		body: v.string(),
+		to: v.string(),
+		badge: v.optional(v.number()),
+		data: v.record(v.string(), v.any()),
+		priority: v.optional(v.union(v.literal('high'), v.literal('normal'), v.literal('default'))),
+		subtitle: v.optional(v.string()),
+		ttl: v.optional(v.number()),
+		expiration: v.optional(v.number()),
+		interruptionLevel: v.optional(v.union(v.literal('active'), v.literal('passive'), v.literal('critical'), v.literal('time-sensitive'))),
+	},
+	async handler(ctx, args) {
+		await pushNotifications.sendPushNotification(ctx, {
+			userId: args.to,
+			notification: {
+				body: args.body,
+				title: args.title,
+				badge: args.badge,
+				data: args.data,
+				priority: args.priority,
+				subtitle: args.subtitle,
+				ttl: args.ttl,
+				expiration: args.expiration,
+				interruptionLevel: args.interruptionLevel,
+			},
+		})
 	},
 })
